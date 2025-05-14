3a3414384324449e8c39527bc4d9abe6988ff1c8
pan-don <afan27027@gmail.com>
2025-05-14 21:10:45 +0700

    create error hanling and comment code

--- app/preprocessing/pipeline.py
+++
@@ -1,6 +0,0 @@
-from app.preprocessing.text_processing import Preprocessing
-
-def Pipeline(input_text: str, inference: bool=True) -> str:
-    clean_text = Preprocessing(input_text=input_text, inference=inference)
-    result = clean_text.pipeline()
-    return result
